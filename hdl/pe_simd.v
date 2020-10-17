7ccbdd8bd2a0cd52d62a3565cbfc24a026592c28
louislxw <louislxw@gmail.com>
2020-10-17 16:57:30 +0800

    Adding SIMD fashion PE with its testbench...

---
+++ hdl/pe_simd.v
@@ -0,0 +1,172 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 13.10.2020 18:47:18
+// Design Name: 
+// Module Name: pe_simd
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+`include "parameters.vh"
+
+module pe_simd( 
+    clk, rst, din_v, din_pe, inst_in_v, inst_in, dout_v, dout_pe
+    );
+    
+input  clk;
+input  rst;
+input  din_v;
+input  [`DATA_WIDTH*2-1:0] din_pe;
+input  inst_in_v;
+input  [`INST_WIDTH-1:0] inst_in;
+
+output dout_v;
+output [`DATA_WIDTH*2-1:0] dout_pe;
+
+wire inst_out_v;
+wire [`INST_WIDTH-1:0] inst_pc; // instructions triggered by program counter
+
+wire [`ALUMODE_WIDTH*4-1:0] alumode; // 4-bit * 4
+wire [`INMODE_WIDTH*4-1:0] inmode;   // 5-bit * 4 
+wire [`OPMODE_WIDTH*4-1:0] opmode;   // 7-bit * 4
+wire [3:0] cea2;    // 1-bit * 4
+wire [3:0] ceb2;    // 1-bit * 4
+wire [3:0] usemult; // 1-bit * 4
+
+wire [`DATA_WIDTH*2-1:0] dout_ctrl;
+wire [`DATA_WIDTH*2-1:0] rdata0, rdata1; 
+wire [`DATA_WIDTH*2-1:0] dout_alu;
+
+parameter DELAY = 14;
+reg shift_v = 0; 
+reg [DELAY-1:0] shift_reg_v = 0;
+always @ (posedge clk) begin 
+    shift_reg_v <= {shift_reg_v[DELAY-2:0], shift_v};
+end
+wire shift_v_d;
+assign shift_v_d = shift_reg_v[DELAY-1]; // valid signal for shift registers
+
+/*** shift register array for din_pe ***/
+reg [`REG_ADDR_WIDTH-1:0] dc = 0; // data counter for shift_reg
+reg [`DATA_WIDTH*2-1:0] shift_reg_data [0:`REG_NUM-1]; 
+reg [`DATA_WIDTH*2-1:0] din_ctrl;
+reg [`REG_ADDR_WIDTH-1:0] addr = 0;
+integer i;
+always @ (posedge clk) begin 
+    if (din_v && dc <= `REG_NUM-1) begin  // should last for 32 cycles
+        for(i = `REG_NUM-1; i > 0; i = i-1) begin
+            shift_reg_data[i] <= shift_reg_data[i-1];
+        end
+        din_ctrl <= din_pe; // MUX
+        shift_reg_data[0] <= din_pe;
+        dc <= dc + 1;
+        shift_v <= 1;
+    end
+    else if (din_v && dc > `REG_NUM-1) begin
+        din_ctrl <= shift_reg_data[2**`REG_ADDR_WIDTH-addr]; // MUX
+        addr <= addr + 1; // MUX
+        shift_v <= 0;
+//        dc <= 0;
+    end
+    else if (shift_v_d) begin // should last for 32 cycles
+        for(i = `REG_NUM-1; i > 0; i = i-1) begin
+            shift_reg_data[i] <= shift_reg_data[i-1];
+        end
+        shift_reg_data[0] <= din_pe;
+    end
+    else begin
+        din_ctrl <= 0; // MUX
+        shift_v <= 0;
+    end
+        
+end
+
+
+// Instruction Memory
+inst_mem IMEM(
+    .clk(clk), 
+    .rst(rst), 
+    .inst_in_v(inst_in_v), 
+    .inst_in(inst_in), 
+    .inst_out_v(inst_out_v),
+    .inst_out(inst_pc) // instructions triggered by program counter
+    ); 
+
+// Control Logics & Decoder
+control CTRL(
+    .clk(clk),
+    .din_ld(din_ctrl), 
+    .din_wb(dout_pe), 
+    .inst_v(inst_out_v),
+    .inst(inst_pc), // instructions triggered by program counter
+    .dout_v(dout_v),
+    .dout(dout_ctrl), // input data of DMEM
+    .alumode(alumode), 
+    .inmode(inmode), 
+    .opmode(opmode), 
+    .cea2(cea2), 
+    .ceb2(ceb2), 
+    .usemult(usemult)
+    );
+
+//wire wren, rden; // write enable & read enable signal for DMEM
+//assign wren = din_v | reg_v;
+//assign rden = inst_out_v ? inst_pc[`INST_WIDTH-5] : 0; // bit: 59
+
+reg wren, rden; // register write/read enable signal to synchronize with dout_ctrl
+reg [`REG_ADDR_WIDTH-1:0] dmem_count = 0; // counter for data memory
+always @ (posedge clk) begin
+    if (din_v && dmem_count <= `REG_NUM*2-1) begin
+        wren <= 1;
+        dmem_count <= dmem_count + 1;
+    end
+    else
+        wren <= 0;
+    
+    if (inst_out_v)
+        rden <= inst_pc[`INST_WIDTH-5]; // bit: 59
+    else
+        rden <= 0;
+end
+
+// Data Memory
+data_mem DMEM(
+    .clk(clk), 
+    .rst(rst), 
+    .wren(wren), // valid for din_pe
+    .wben(dout_v), // valid for dout_pe
+    .rden(rden), 
+    .inst_v(inst_out_v),
+    .inst(inst_pc), // instructions triggered by program counter
+    .wdata(dout_ctrl), 
+    .rdata0(rdata0),
+    .rdata1(rdata1)
+    );
+
+// ALU for Complex Data
+complex_alu ALU( 
+    .clk(clk), 
+    .rst(rst), 
+    .alumode(alumode), 
+    .inmode(inmode), 
+    .opmode(opmode), 
+    .cea2(cea2), 
+    .ceb2(ceb2), 
+    .usemult(usemult),
+    .din_1(rdata0), 
+    .din_2(rdata1), 
+    .dout(dout_pe) 
+    );    
+    
+endmodule
---
+++ sim/tb_pe_simd.v
@@ -0,0 +1,136 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 17.10.2020 16:49:07
+// Design Name: 
+// Module Name: tb_pe_simd
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+`include "parameters.vh"
+
+module tb_pe_simd;
+
+    // Inputs
+    reg clk;
+    reg rst;
+    reg din_v;
+    reg [`DATA_WIDTH*2-1:0] din_pe;
+    reg inst_in_v;
+    reg [`INST_WIDTH-1:0] inst_in;
+    
+    // Outputs
+    wire dout_v;
+    wire [`DATA_WIDTH*2-1:0] dout_pe; 
+    
+    // Instantiate the Unit Under Test (UUT)
+    pe_simd uut(
+    .clk(clk), 
+    .rst(rst), 
+    .din_v(din_v),
+    .din_pe(din_pe),
+    .inst_in_v(inst_in_v), 
+    .inst_in(inst_in),
+    .dout_v(dout_v), 
+    .dout_pe(dout_pe)
+    ); 
+    
+    parameter PERIOD = 20;
+
+    always begin
+        clk = 1'b0;
+        #(PERIOD/2) clk = 1'b1;
+        #(PERIOD/2);
+    end
+    
+    integer cycle;
+    
+    initial cycle = 0;
+    always @(posedge clk)
+        cycle = cycle + 1;
+    
+    initial begin
+        // Initialize Inputs
+        clk = 0;
+        rst = 0;
+        din_v = 0;
+        din_pe = 0;
+        inst_in_v = 0;
+        
+        // Wait 100 ns for global reset to finish
+        rst = 1;
+        #100;
+        
+        // Add stimulus here
+        #20; rst = 0; 
+		#20;
+		#20;
+
+//		#20; inst_in_v = 1; inst_in = 64'h0_0_0000000_0_00_00_00; // din_ld
+//		#20; inst_in_v = 1; inst_in = 64'h0_0_0000000_0_01_00_00; // din_ld
+// 		#20; inst_in_v = 1; inst_in = 64'h0_0_0000000_0_02_00_00; // din_ld
+//		#20; inst_in_v = 1; inst_in = 64'h4_0_0000000_0_03_00_00; // din_pe
+//		#20; inst_in_v = 1; inst_in = 64'h4_0_0000000_0_04_00_00; // din_pe
+//		#20; inst_in_v = 1; inst_in = 64'h4_0_0000000_0_05_00_00; // din_pe
+//		#20; inst_in_v = 1; inst_in = 64'h0_8_0000000_3_00_01_00; // CMPLX_MULT
+//		#20; inst_in_v = 1; inst_in = 64'h0_8_0000000_3_00_03_02; // CMPLX_MULT
+//		#20; inst_in_v = 1; inst_in = 64'h0_8_0000000_3_00_05_04; // CMPLX_MULT
+
+        // Load the instructions
+        #20; // inst_in_v = 1; inst_in = 64'h0_0_0000000_0_00_00_00; // din_pe
+        #20; // inst_in_v = 1; inst_in = 64'h0_0_0000000_0_01_00_00; // din_pe
+        #20; // inst_in_v = 1; inst_in = 64'h0_0_0000000_0_02_00_00; // din_pe
+		#20; // inst_in_v = 1; inst_in = 64'h0_0_0000000_0_03_00_00; // din_pe
+		#20; // inst_in_v = 1; inst_in = 64'h0_0_0000000_0_04_00_00; // din_pe
+		#20; // inst_in_v = 1; inst_in = 64'h0_0_0000000_0_05_00_00; // din_pe
+		#20; inst_in_v = 1; inst_in = 64'h8_8_0000000_3_80_01_00; // CMPLX_MULT (inst rden & data WB)
+		#20; inst_in_v = 1; inst_in = 64'h8_8_0000000_3_81_03_02; // CMPLX_MULT (inst rden & data WB)
+		#20; inst_in_v = 1; inst_in = 64'h8_8_0000000_3_82_05_04; // CMPLX_MULT (isnt rden & data WB)
+		
+//		#20; inst_in_v = 0; inst_in = 0; din_v = 0;
+//		#20; inst_in_v = 0; inst_in = 0; din_v = 0;
+//		#20; inst_in_v = 0; inst_in = 0; din_v = 1; din_ld = 32'h0004_0002;  din_pe = 32'd2; // 4 + j*2 
+//		#20; inst_in_v = 0; inst_in = 0; din_v = 1; din_ld = 32'h0003_0001;  din_pe = 32'd4; // 3 + j*1
+//		#20; inst_in_v = 0; inst_in = 0; din_v = 1; din_ld = 32'h0008_0006;  din_pe = 32'd6; // 8 + j*6 
+//		#20; inst_in_v = 0; inst_in = 0; din_v = 1; din_ld = 32'd7;  din_pe = 32'h0007_0005; // 7 + j*5 
+//		#20; inst_in_v = 0; inst_in = 0; din_v = 1; din_ld = 32'd9;  din_pe = 32'h000c_000a; // 12 + j*10
+//		#20; inst_in_v = 0; inst_in = 0; din_v = 1; din_ld = 32'd11; din_pe = 32'h000b_0009; // 11 + j*9
+
+        // Load the data
+		#20; inst_in_v = 0; inst_in = 0; din_v = 0;
+		#20; inst_in_v = 0; inst_in = 0; din_v = 0;
+		#20; inst_in_v = 0; inst_in = 0; din_v = 1; din_pe = 32'h0004_0002; // 4 + j*2 
+		#20; inst_in_v = 0; inst_in = 0; din_v = 1; din_pe = 32'h0003_0001; // 3 + j*1
+		#20; inst_in_v = 0; inst_in = 0; din_v = 1; din_pe = 32'h0008_0006; // 8 + j*6 
+		#20; inst_in_v = 0; inst_in = 0; din_v = 1; din_pe = 32'h0007_0005; // 7 + j*5 
+		#20; inst_in_v = 0; inst_in = 0; din_v = 1; din_pe = 32'h000c_000a; // 12 + j*10
+		#20; inst_in_v = 0; inst_in = 0; din_v = 1; din_pe = 32'h000b_0009; // 11 + j*9
+		
+//		#20; din_v = 0; din_ld = 32'd0;  din_pe = 0; 
+//		#20; din_v = 0; din_ld = 32'd0;  din_pe = 0; 
+//		#20; din_v = 0; din_ld = 32'd0;  din_pe = 0; 
+//		#20; din_v = 0; din_ld = 32'd0;  din_pe = 0; 
+
+		#20; din_v = 0; din_pe = 1; 
+		#20; din_v = 0; din_pe = 2; 
+		#20; din_v = 0; din_pe = 3; 
+		#20; din_v = 0; din_pe = 4; 
+		#20; din_v = 0; din_pe = 5; 
+		#20; din_v = 0; din_pe = 6; 
+						
+		#1000;
+		
+    end    
+
+endmodule
