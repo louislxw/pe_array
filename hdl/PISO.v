61dfcd1c473a87a0ee3fda41aae824079d6df7c1
louislxw <louislxw@gmail.com>
2020-11-16 17:23:50 +0800

    These HDLs meet timing but need to be verified by simulation

--- hdl/PISO.v
+++ hdl/PISO.v
@@ -3,15 +3,15 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 06.10.2020 09:48:05
+// Create Date: 11.11.2020 16:42:49
 // Design Name: 
 // Module Name: PISO
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
-// Description: Parallel-in to Serial-out (PISO) Shift Register
+// Description: Parallel-in to Serial-out (PISO) 
 // 
-// Dependencies: 
+// Dependencies: 8147 LUTs, 8193 FFs
 // 
 // Revision:
 // Revision 0.01 - File Created
@@ -21,42 +21,52 @@
 `include "parameters.vh"
 
 module PISO(
-    clk, rst, fetch, p_in, s_out
+    clk, rst, ce, load, p_in, s_out_v, s_out
     );
     
 input clk;
 input rst;
-input fetch; // fetch parallel ouput
-input [`REG_NUM*`DATA_WIDTH*2-1:0] p_in;
+input ce;
+input load; // load parallel ouput into SRL
+input [`PE_NUM*`DATA_WIDTH*2-1:0] p_in;
 
+output s_out_v;
 output reg [`DATA_WIDTH*2-1:0] s_out; 
 
-wire [`DATA_WIDTH*2-1:0] array_output [0:`REG_NUM-1]; 
+wire [`DATA_WIDTH*2-1:0] array_output [0:`PE_NUM-1]; 
 genvar j;
-for(j = 0; j < `REG_NUM; j = j+1) begin 
+for(j = 0; j < `PE_NUM; j = j+1) begin 
     assign array_output[j] = p_in[(j+1)*`DATA_WIDTH*2-1:j*`DATA_WIDTH*2];
 end
 
-reg [`DATA_WIDTH*2-1:0] shift_reg_data [0:`REG_NUM-1]; 
+reg [`DATA_WIDTH*2-1:0] shift_reg_data [0:`PE_NUM-1]; 
 
 integer i;
 always @ (posedge clk) begin
     if (rst)
-        for(i = `REG_NUM-1; i > 0; i = i-1) begin 
+        for(i = `PE_NUM-1; i > 0; i = i-1) begin 
             shift_reg_data[i] <= 0;
+            s_out <= 0;
         end
-    else if (fetch) begin
-        s_out <= shift_reg_data[`REG_NUM-1]; 
-        for(i = `REG_NUM-1; i > 0; i = i-1) begin 
-            shift_reg_data[i] <= shift_reg_data[i-1];
+    else if (load) begin
+        for(i = `PE_NUM-1; i > 0; i = i-1) begin 
+            shift_reg_data[i] <= array_output[i];
         end
     end
-    else begin
-        for(i = `REG_NUM-1; i > 0; i = i-1) begin 
-            shift_reg_data[i] <= array_output[i];
+    else if (ce) begin // left shift
+        s_out <= shift_reg_data[0]; 
+        for(i = 0; i < `PE_NUM-1; i = i+1) begin 
+            shift_reg_data[i] <= shift_reg_data[i+1];
         end
     end
 
 end
+
+parameter DELAY = `PE_NUM;
+reg [DELAY-1:0] shift_reg_v = 0;
+always @ (posedge clk) begin 
+    shift_reg_v <= {shift_reg_v[DELAY-2:0], ce};
+end
+assign s_out_v = shift_reg_v[DELAY-1]; // valid signal for shift registers
 
 endmodule
---
+++ hdl/stream_io.v
@@ -0,0 +1,68 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11.11.2020 15:54:56
+// Design Name: 
+// Module Name: stream_io
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: SIPO to PISO --> SISO
+// 
+// Dependencies: 16,378 LUTs, 16,431 FFs (meet 500MHz)
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+`include "parameters.vh"
+
+module stream_io(
+    clk, rst, p_ce, load, s_in_v, s_in, s_out_v, s_out
+    );
+    
+input clk;
+input rst;
+input s_in_v;
+input p_ce;
+input load;
+input [`DATA_WIDTH*2-1:0] s_in;
+
+output s_out_v;
+output [`DATA_WIDTH*2-1:0] s_out;
+
+// intermediate signals
+wire p_out_v;
+wire [`PE_NUM*`DATA_WIDTH*2-1:0] p_out;
+
+sipo_y in_buffer(
+    .clk(clk), 
+    .s_in_v(s_in_v), 
+    .s_in(s_in), 
+    .p_out_v(p_out_v),
+    .p_out(p_out)
+    );
+    
+PISO out_buffer( 
+    .clk(clk), 
+    .rst(rst), 
+    .ce(p_ce),
+    .load(load),
+    .p_in(p_out), 
+    .s_out_v(s_out_v),
+    .s_out(s_out) 
+    );
+
+// Something was wrong with this module when integrating with top module
+//piso_new out_buffer(
+//    .clk(clk), 
+//    .p_in_v(p_out_v), 
+//    .p_in(p_out), 
+//    .s_out_v(s_out_v), 
+//    .s_out(s_out)
+//    );
+    
+endmodule
