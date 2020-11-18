827c3854810d920f47eed24077ae871192a6a483
louislxw <louislxw@gmail.com>
2020-11-18 23:18:41 +0800

    Add piso macro block

---
+++ hdl/piso_macro.v
@@ -0,0 +1,53 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 18.11.2020 21:46:37
+// Design Name: 
+// Module Name: piso_macro
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+`include "parameters.vh"
+
+module piso_macro(
+    clk, rst, load, ce, p_in, s_out
+    );
+
+input clk;
+input rst;
+input load;
+input ce;
+input [`DATA_WIDTH*2-1:0] p_in;
+output s_out;
+    
+   parameter piso_shift = `DATA_WIDTH*2;
+
+   reg [piso_shift-2:0] shift_reg = {piso_shift-1{1'b0}};
+   reg                      s_out = 1'b0;
+
+   always @(posedge clk)
+      if (rst) begin
+         shift_reg <= 0;
+         s_out     <= 1'b0;
+      end
+      else if (load) begin
+         shift_reg <= p_in[piso_shift-1:1];
+         s_out     <= p_in[0];
+      end
+      else if (ce) begin
+         shift_reg <= {1'b0, shift_reg[piso_shift-2:1]};
+         s_out     <= shift_reg[0];
+      end		
+    
+endmodule
---
+++ sim/tb_piso_macro.v
@@ -0,0 +1,82 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 18.11.2020 22:36:15
+// Design Name: 
+// Module Name: tb_piso_macro
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb_piso_macro; 
+
+    // Inputs
+    reg clk;
+    reg rst;
+    reg load;
+    reg ce;
+    reg [`DATA_WIDTH*2-1:0] p_in;
+    
+    // Outputs
+    wire s_out; 
+    
+    // Instantiate the Unit Under Test (UUT)
+    piso_macro uut(
+    .clk(clk), 
+    .rst(rst),
+    .load(load),
+    .ce(ce), 
+    .p_in(p_in),
+    .s_out(s_out)
+    ); 
+
+    parameter PERIOD = 20;
+
+    always begin
+        clk = 1'b0;
+        #(PERIOD/2) clk = 1'b1;
+        #(PERIOD/2);
+    end
+    
+    integer cycle;
+    
+    initial cycle = 0;
+    always @(posedge clk)
+        cycle = cycle + 1;
+
+     initial begin
+        // Initialize Inputs
+        clk = 0;
+        rst = 1;
+        load = 0;
+        ce = 0;
+        p_in = 0;
+        
+        // Wait 100 ns for global reset to finish
+        #100;
+        rst = 0;
+        // Add stimulus here
+        #20; load = 1; p_in = 32'h0100_0001; 
+        #20; load = 1; p_in = 32'h0100_0000; 
+        #20; load = 1; p_in = 32'h0100_0001; 
+        #20; load = 0; ce = 1;
+        #640; 
+        #20; ce = 0; p_in = 32'd0;
+        
+        #1000;
+        
+     end   
+
+endmodule
