92c3bb5e6049beff1089250ffbb0f0015a678c91
louislxw <louislxw@gmail.com>
2021-03-08 16:24:06 +0800

    Add PE array files

---
+++ hdl/pe_array.v
@@ -0,0 +1,160 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/22/2021 01:59:11 PM
+// Design Name: 
+// Module Name: pe_array
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+`include "parameters.vh"
+
+module pe_array(
+    clk, rst, load, din_overlay_v, din_overlay, dout_overlay_v, dout_overlay
+    );
+    
+input  clk; 
+input  rst; 
+input  load; 
+input  din_overlay_v; 
+input  [`DATA_WIDTH*2-1:0] din_overlay; 
+
+output dout_overlay_v; 
+output [`DATA_WIDTH*2-1:0] dout_overlay; 
+//reg [`DATA_WIDTH*2-1:0] dout_overlay; //
+
+reg  [`PE_NUM-1:0] pe_in_v;
+reg  [`DATA_WIDTH*2-1:0] pe_in [`PE_NUM-1:0];
+wire [`PE_NUM-1:0] pe_out_v;
+wire [`DATA_WIDTH*2-1:0] pe_out [`PE_NUM-1:0];
+wire [`PE_NUM-1:0] pe_tx_v;
+wire [`DATA_WIDTH*2-1:0] pe_tx [`PE_NUM-1:0];
+wire [`PE_NUM-1:0] pe_shift_v;
+wire [`DATA_WIDTH*2-1:0] pe_shift [`PE_NUM-1:0];
+wire p_in_v;
+wire [`PE_NUM*`DATA_WIDTH*2-1:0] p_in;
+//wire [`PE_NUM*`DATA_WIDTH*2-1:0] pe_in;
+//wire p_out_v;
+//wire shift_v;
+
+//sipo_y in_buffer(
+//    .clk(clk), 
+//    .rst(rst),
+//    .shift_v(shift_v),
+//    .s_in_v(din_overlay_v), 
+//    .s_in(din_overlay), 
+//    .p_out_v(p_out_v),
+//    .p_out(pe_in)
+//    );
+
+reg [4:0] load_cnt = 0;
+reg [13:0] stream_cnt = 0;
+always @ (posedge clk) 
+    if(din_overlay_v) 
+        load_cnt <= load_cnt + 1;
+    else 
+        load_cnt <= 0;
+
+integer j;
+always @ (posedge clk) begin
+    for (j = 0; j < `PE_NUM; j = j + 1) begin 
+        if(load_cnt >= j*`LOAD_NUM && load_cnt < (j+1)*`LOAD_NUM) begin
+            pe_in_v[j] <= din_overlay_v;
+            pe_in[j] <= din_overlay;
+        end
+        else begin
+            pe_in_v[j] <= 0;
+            pe_in[j] <= 0;
+        end
+    end
+end
+
+
+genvar i;
+generate
+    for (i = 0; i < `PE_NUM; i = i + 1) begin : array
+        // PE_0
+        if (i == 0) begin
+            pe PE0( 
+            .clk(clk), 
+            .rst(rst), 
+            .din_pe_v(pe_in_v[0]), 
+            .din_pe(pe_in[0]), 
+            .din_shift_v(pe_shift_v[0]), 
+            .din_shift(pe_shift[0]),
+            .din_tx_v(0), 
+            .din_tx(0), 
+            .dout_pe_v(pe_out_v[0]), 
+            .dout_pe(pe_out[0]), 
+            .dout_tx_v(pe_tx_v[0]), 
+            .dout_tx(pe_tx[0]),
+            .dout_shift_v(), 
+            .dout_shift()
+            );
+        end       
+        // PE_N-1 (Output)
+        else if (i == `PE_NUM-1) begin
+            pe PE_N_1( 
+            .clk(clk), 
+            .rst(rst), 
+            .din_pe_v(pe_in_v[i]), 
+            .din_pe(pe_in[i]), 
+            .din_shift_v(), 
+            .din_shift(),   
+            .din_tx_v(pe_tx_v[i]), 
+            .din_tx(pe_tx[i]),  
+            .dout_pe_v(pe_out_v[i]), 
+            .dout_pe(pe_out[i]), 
+            .dout_tx_v(pe_tx_v[i]), 
+            .dout_tx(pe_tx[i]),
+            .dout_shift_v(pe_shift_v[i-1]), 
+            .dout_shift(pe_shift[i-1])
+            );
+        end        
+        // PE_1 to PE_N-2
+        else begin
+             pe PE_K( 
+            .clk(clk), 
+            .rst(rst), 
+            .din_pe_v(pe_in_v[i]), 
+            .din_pe(pe_in[i]), 
+            .din_shift_v(pe_shift_v[i]), 
+            .din_shift(pe_shift[i]),            
+            .din_tx_v(pe_tx_v[i-1]), 
+            .din_tx(pe_tx[i-1]), 
+            .dout_pe_v(pe_out_v[i]), 
+            .dout_pe(pe_out[i]), 
+            .dout_tx_v(pe_tx_v[i]), 
+            .dout_tx(pe_tx[i]),
+            .dout_shift_v(pe_shift_v[i-1]), 
+            .dout_shift(pe_shift[i-1])
+            ); 
+        end
+        
+        assign p_in[(i+1)*`DATA_WIDTH*2-1:i*`DATA_WIDTH*2] = pe_out[i];
+//        assign p_in_v = pe_out_v[i];
+        
+    end
+endgenerate
+
+piso_new out_buffer(
+    .clk(clk), 
+    .load(load),
+    .p_in_v(), //
+    .p_in(p_in), 
+    .s_out_v(dout_overlay_v), 
+    .s_out(dout_overlay)
+    );
+    
+endmodule
---
+++ sim/tb_pe_array.v
@@ -0,0 +1,126 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/08/2021 01:30:14 PM
+// Design Name: 
+// Module Name: tb_pe_array
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+`include "parameters.vh"
+
+module tb_pe_array;
+
+    // Inputs
+    reg clk; 
+    reg rst; 
+    reg load; 
+    reg din_overlay_v; 
+    reg [`DATA_WIDTH*2-1:0] din_overlay; 
+
+    wire dout_overlay_v; 
+    wire [`DATA_WIDTH*2-1:0] dout_overlay; 
+
+    // Instantiate the Unit Under Test (UUT)
+    pe_array uut(
+    .clk(clk), 
+    .rst(rst), 
+    .load(load), 
+    .din_overlay_v(din_overlay_v), 
+    .din_overlay(din_overlay), 
+    .dout_overlay_v(dout_overlay_v), 
+    .dout_overlay(dout_overlay)
+    );
+
+    parameter PERIOD = 20;
+
+    always begin
+        clk = 1'b0;
+        #(PERIOD/2) clk = 1'b1;
+        #(PERIOD/2);
+    end
+    
+    integer cycle;
+    
+    initial cycle = 0;
+    always @(posedge clk)
+        cycle = cycle + 1;
+
+    initial begin
+        // Initialize Inputs
+        clk = 0;
+        load = 0;
+        din_overlay_v = 0;
+        din_overlay = 0; 
+        
+        // Wait 100 ns for global reset to finish
+        rst = 1;
+        #100;
+        
+        // Add stimulus here
+        #20; rst = 0; 
+        #20;
+        #20;
+
+        // Load the data
+        #20; din_overlay_v = 0; 
+        #20; din_overlay_v = 0;
+        #20; din_overlay_v = 1; din_overlay = 32'h0004_0002; // 4 + j*2 
+        #20; din_overlay_v = 1; din_overlay = 32'h0003_0001; // 3 + j*1
+        #20; din_overlay_v = 1; din_overlay = 32'h0008_0006; // 8 + j*6 
+        #20; din_overlay_v = 1; din_overlay = 32'h0007_0005; // 7 + j*5 
+        #20; din_overlay_v = 1; din_overlay = 32'h000c_000a; // 12 + j*10
+        #20; din_overlay_v = 1; din_overlay = 32'h000b_0009; // 11 + j*9
+        #20; din_overlay_v = 1; din_overlay = 32'h0004_0002; // 4 + j*2 // add
+        #20; din_overlay_v = 1; din_overlay = 32'h0003_0001; // 3 + j*1 // add 
+
+        #20; din_overlay_v = 1; din_overlay = 32'h0001_0001; // 1 + j*1 
+        #20; din_overlay_v = 1; din_overlay = 32'h0002_0002; // 2 + j*2
+        #20; din_overlay_v = 1; din_overlay = 32'h0003_0003; // 3 + j*3 
+        #20; din_overlay_v = 1; din_overlay = 32'h0004_0004; // 4 + j*4 
+        #20; din_overlay_v = 1; din_overlay = 32'h0005_0005; // 5 + j*5
+        #20; din_overlay_v = 1; din_overlay = 32'h0006_0006; // 6 + j*6
+        #20; din_overlay_v = 1; din_overlay = 32'h0007_0007; // 7 + j*7
+        #20; din_overlay_v = 1; din_overlay = 32'h0008_0008; // 8 + j*8 
+
+        #20; din_overlay_v = 1; din_overlay = 32'h0009_0009; 
+        #20; din_overlay_v = 1; din_overlay = 32'h000a_000a; 
+        #20; din_overlay_v = 1; din_overlay = 32'h000b_000b; 
+        #20; din_overlay_v = 1; din_overlay = 32'h000c_000c; 
+        #20; din_overlay_v = 1; din_overlay = 32'h000d_000d; 
+        #20; din_overlay_v = 1; din_overlay = 32'h000e_000e; 
+        #20; din_overlay_v = 1; din_overlay = 32'h000f_000f; 
+        #20; din_overlay_v = 1; din_overlay = 32'h0010_0010; 
+
+        #20; din_overlay_v = 1; din_overlay = 32'h0001_0001; // 1 + j*1 
+        #20; din_overlay_v = 1; din_overlay = 32'h0002_0002; // 2 + j*2
+        #20; din_overlay_v = 1; din_overlay = 32'h0003_0003; // 3 + j*3 
+        #20; din_overlay_v = 1; din_overlay = 32'h0004_0004; // 4 + j*4 
+        #20; din_overlay_v = 1; din_overlay = 32'h0005_0005; // 5 + j*5
+        #20; din_overlay_v = 1; din_overlay = 32'h0006_0006; // 6 + j*6
+        #20; din_overlay_v = 1; din_overlay = 32'h0007_0007; // 7 + j*7
+        #20; din_overlay_v = 1; din_overlay = 32'h0008_0008; // 8 + j*8 
+                
+        #20; din_overlay_v = 0; din_overlay = 1; 
+        #20; din_overlay_v = 0; din_overlay = 2; 
+        #20; din_overlay_v = 0; din_overlay = 3; 
+        #20; din_overlay_v = 0; din_overlay = 4; 
+        #20; din_overlay_v = 0; din_overlay = 5; 
+        #20; din_overlay_v = 0; din_overlay = 6; 
+        
+        #2000;
+		
+    end   
+
+endmodule
